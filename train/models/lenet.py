57b403799c1bc00ea986c4f4e873f0f109d03dc1
Sibylse <45455846+Sibylse@users.noreply.github.com>
2022-09-29 14:56:09 +0200

    Create lenet.py

---
+++ train/models/lenet.py
@@ -0,0 +1,35 @@
+'''LeNet in PyTorch.'''
+import torch.nn as nn
+import torch.nn.functional as F
+
+# The embedding architecture returning the 
+# output of the penultimate layer
+class LeNetEmbed(nn.Module):
+    def __init__(self,embedding_dim=84):
+        super(LeNetEmbed, self).__init__()
+        self.conv1 = nn.Conv2d(1, 6, 5)
+        self.conv2 = nn.Conv2d(6, 16, 5)
+        self.fc1   = nn.Linear(16*4*4, 120)
+        self.fc2   = nn.Linear(120, embedding_dim)
+
+    def forward(self, x):
+        out = F.relu(self.conv1(x))
+        out = F.max_pool2d(out, 2)
+        out = F.relu(self.conv2(out))
+        out = F.max_pool2d(out, 2)
+        out = out.view(out.size(0), -1)
+        out = F.relu(self.fc1(out))
+        out = F.relu(self.fc2(out))
+        return out
+    
+    
+class LeNet(nn.Module):
+    def __init__(self,embedding_dim, classifier):
+        super(LeNet, self).__init__()
+        self.embed = LeNetEmbed(embedding_dim=embedding_dim)
+        self.classifier = classifier
+
+    def forward(self, x):
+        out = self.embed(x)
+        out = self.classifier(out)
+        return out
